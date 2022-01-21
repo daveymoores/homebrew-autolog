4f849a4d4fa783cdf56792a79168797014a7a291
Davey Moores <daveymoores@gmail.com>
2022-01-21 12:54:23 +0100

    0.1.1-alpha release

--- Formula/autolog.rb
+++ Formula/autolog.rb
@@ -4,9 +4,9 @@
 class Autolog < Formula
   desc "A commit-log based timesheet generator, written in Rust"
   homepage "https://github.com/daveymoores/autolog"
-  url "https://github.com/daveymoores/autolog/releases/download/0.1.0-alpha/autolog-mac.tar.gz"
+  url "https://github.com/daveymoores/autolog/releases/download/0.1.1-alpha/autolog-mac.tar.gz"
   sha256 "1138a0f884208ba79326ec27725e0adc547c3e6272d64d8fda5b56a0085f4557"
-  version "0.1.0-alpha"
+  version "0.1.1-alpha"
 
   def install
     bin.install "autolog"
