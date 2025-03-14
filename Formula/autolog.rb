b5155873d1b14dd909512525ba3a1a49a3252cf0
David Moores <daveymoores@gmail.com>
2025-03-14 16:55:07 +0100

    chore: update formula to v0.2.1-beta

--- Formula/autolog.rb
+++ Formula/autolog.rb
@@ -1,7 +1,7 @@
 class Autolog < Formula
   desc "CLI tool for automated logging"
   homepage "https://github.com/daveymoores/autolog"
-  url "https://github.com/daveymoores/autolog/releases/download/v0.2.0-beta/autolog-mac.tar.gz"
+  url "https://github.com/daveymoores/autolog/releases/download/v0.2.1-beta/autolog-mac.tar.gz"
   sha256 "406413f1466c096a502b7aa001b65e2610bbb2a13303f464b89389465bffcabd"
   license "MIT"
 
