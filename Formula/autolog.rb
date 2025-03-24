d447ca0eb3784e7097e968d646bed5531008d9a1
David Moores <daveymoores@gmail.com>
2025-03-24 12:58:48 +0100

    fix: remove uninstall script

--- Formula/autolog.rb
+++ Formula/autolog.rb
@@ -1,5 +1,5 @@
 class Autolog < Formula
-  desc "CLI tool for automated logging"
+  desc "Git-based CLI tool for timesheet generation"
   homepage "https://github.com/daveymoores/autolog"
   url "https://github.com/daveymoores/autolog/releases/download/v0.3.69/autolog-mac.tar.gz"
   sha256 "d4fb4e5379942acbb165384ee28aa69237d72ffe4aa16014a6ff7d8617149d4c"
@@ -8,14 +8,6 @@ class Autolog < Formula
   def install
     bin.install "autolog" # Directly install the binary
     (var/"autolog").mkpath
-  end
-
-  def post_uninstall
-    db_dir = "#{ENV["HOME"]}/Library/Application Support/dev.autolog.cli"
-    if Dir.exist?(db_dir)
-      puts "Removing database directory at #{db_dir}"
-      system "rm", "-rf", db_dir
-    end
   end
 
   test do
